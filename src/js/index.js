78390cd734655b3e5a003a2c7b62d5a878464487
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-09-21 22:56:35 +0600

    babel modules setup

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,5 @@
 
 // Global app controller
 import num from './test';
-
-console.log(`I imported ${num} from another module`);
+const x = 44;
+console.log(`I imported ${num} from another module. Variable x is ${x}`);
