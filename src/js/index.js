724aef6c15cb2658302f2e521b07dc78246ca400
Md. Ayub Khan <aiubkhan95@gmail.com>
2018-09-23 13:15:52 +0600

    rendering loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-import { elements } from "./views/base";
+import { elements, renderLoader, clearLoader } from "./views/base";
 /** Global state of the app
  * _ Search object
  * _ Current recipe object
@@ -21,9 +21,11 @@ const controlSearch = async () => {
     // 3. Prepare UI for results
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
     // 4. Search for recipes
     await state.search.getResults();
     // 5. Render result on UI
+    clearLoader();
     searchView.renderResults(state.search.result);    
   }
 };
